73941efefbb3a130289b71e21dac706cd663f41d
Gilberto Setubal Rodrigues <gsrodrigues@yahoo.com>
2019-02-28 17:59:03 -0300

    Update TesteScanner4Formatacao.java

--- cursos/src/br/com/gilberto/cursos/test/TesteScanner4Formatacao.java
+++ cursos/src/br/com/gilberto/cursos/test/TesteScanner4Formatacao.java
@@ -12,21 +12,32 @@ public class TesteScanner4Formatacao {
 		
 		while (sc.hasNextLine()) {
 			String linha = sc.nextLine();
-			System.out.println(linha);
 
 			Scanner scLinha = new Scanner(linha);
 			scLinha.useDelimiter(",");
-			scLinha.useLocale(Locale.US);			// Define regras de pontuaÁ„o
+			scLinha.useLocale(Locale.US);			// Define regras de pontua√ß√£o
 			
 			String valor1 = scLinha.next();
 			int valor2 = scLinha.nextInt();
 			int valor3 = scLinha.nextInt();
 			String valor4 = scLinha.next();
 			double valor5 = scLinha.nextDouble();	// Sem identificar o ponto decimal, essa linha vai dar erro
+//			System.out.println(valor1 + valor2 + valor3 + valor4 + valor5);
 			
-//			System.out.println(valor1 + valor2 + valor3 + valor4 + valor5);
-			String formatado = String.format("%s", "joe");
-//			System.out.println(valor1 + valor2 + valor3 + valor4 + valor5);
+//			String valorFormatado = String.format("%s %s %s %s %s", valor1, valor2, valor3, valor4, valor5);
+			
+			Integer v2 = new Integer(valor2);
+			Integer v3 = new Integer(valor3);
+			Double v5 = new Double(valor5);
+			Object[] valores = {valor1, v2, v3, valor4, v5};
+			// O par√¢metro Locale.US indica o idioma
+//			String valorFormatado = String.format(Locale.US, "%s - %04d-%08d, %20s: %08.2f", valores);
+//			String valorFormatado = String.format(new Locale("pt"),"%s - %04d-%08d, %20s: %08.2f", valores);
+//			String valorFormatado = String.format("%s - %04d-%08d, %20s: %08.2f", valores);
+//			System.out.println(valorFormatado);
+			
+			System.out.format("%s - %04d-%08d, %20s: %08.2f %n", valores);
+			
 			
 			scLinha.close();
 		}
